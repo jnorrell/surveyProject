bb70e6c4479abad38ddbe7ad19471f6a24fd82a2
unknown <jnorrell2@BEP139-102.patriots.uttyler.edu>
2015-11-17 17:40:53 -0600

    initial commit

---
+++ survey.R
@@ -0,0 +1,27 @@
+## survey project in R
+
+# install and load library
+install.packages("dplyr")
+install.packages("ggplot2")
+library(dplyr)
+library(ggplot2)
+
+# download a file
+download.file("http://files.figshare.com/2236372/combined.csv",  "data/portal_data_joined.csv")
+# import file
+surveys <- read.csv('data/portal_data_joined.csv')
+
+## explore distribution of weight for each species
+
+# data parsing
+
+# build figure
+
+
+## look at composition of data in sampling
+
+
+## explore comparisons between variables
+
+
+
